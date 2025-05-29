b0c4bf93e67589e5055ec590c5ff0c2ee00e8703
Kohei Yamazaki <urigon@gmail.com>
2025-05-29 14:52:20 +0900

    ユーザーからのユーザー登録無効化と匿名ユーザーでの閲覧許可

--- local_settings.py
+++ local_settings.py
@@ -3,3 +3,18 @@ SECURE_SSL_REDIRECT = False
 # Time zone settings
 TIME_ZONE = 'Asia/Tokyo'
 USE_TZ = True 
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'guardian.backends.ObjectPermissionBackend',
+    'tcms.kiwi_auth.backends.AnonymousViewBackend',
+]
+
+ANONYMOUS_USER_PERMISSIONS = (
+    'view_testplan',
+    'view_testcase',
+    'view_testexecution',
+    'view_product',
+    'view_build',
+    'view_environment',
+)
